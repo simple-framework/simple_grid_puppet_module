806f2668f9e18de5c57a3628e553babeea9092d5
maany <imptodefeat@gmail.com>
2018-09-26 16:46:52 +0200

    #5 init.pp cleanup

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,20 +4,8 @@
 #
 # @example
 #   include simple
-class simple_grid(
-	$test_param,
-	$os_param
-	)
-	{
-	file { '/root/test':
-		ensure => present,
-		content => "r10k check configuration",
-	}
-	notify {"aag laga di ${test_param} and ${os_param}":}
-	#include simple_grid::test
-	#notify {"config was ${test_param} and param was ${os_param}":}	
+class simple_grid{
 	Class[simple::pre_conf] -> Class[simple::orchestrator_conf]
-	
 	class{"simple::pre_conf":}
 	class{"simple::orchestrator_conf":}
 }
