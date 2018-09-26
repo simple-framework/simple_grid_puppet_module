c70f7c772d81c535393e090b092a83c7135d63bf
maany <imptodefeat@gmail.com>
2018-09-26 20:15:07 +0200

    add config dir

--- manifests/pre_conf.pp
+++ manifests/pre_conf.pp
@@ -1,3 +1,12 @@
-class simple_grid::pre_conf {
+class simple_grid::pre_conf(
+  $config_dir
+) {
   notify{"Running State: Pre Conf":}
+  #create config directory
+  file {"main config directory":
+    ensure => directory,
+    path   => "${config_dir}", 
+  }
+  #site-level-config-file
+  #run the yaml compiler
 }
