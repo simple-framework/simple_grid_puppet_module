7839f00a38121ae06d5400b701a96c56b6191011
ejr004 <acessoej@icloud.com>
2018-10-16 20:10:32 +0200

    copy site level config file to agent #10

--- manifests/lightweight_component.pp
+++ manifests/lightweight_component.pp
@@ -1,5 +1,14 @@
 class simple_grid::lightweight_component{
   file { "/etc/simple_grid":
-    ensure => present,
+    ensure => directory,
+    mode   => "0644",
+    owner  => "puppet",
   }
+   file { "copy site level config file to agent":
+        mode   => "0644",
+        owner  => "puppet",
+        source => 'puppet:///simple_grid/${site_config_dir}/${site_config_file}',
+        path   => "/etc/simple_grid/simple_grid_site_config_blah"
+    }
+
 }
