4c02fb57de068b3124f8d008e0045c55dbbc47ef
maany <imptodefeat@gmail.com>
2018-10-16 19:50:17 +0200

    #8 ensure present for fileserver.conf

--- manifests/pre_conf.pp
+++ manifests/pre_conf.pp
@@ -14,6 +14,7 @@ class simple_grid::pre_conf(
   }
   #create fileserver.conf
   file {"$confdir/fileserver.conf":
+    ensure  => present,
     path    => "${site_config_dir}",
     content => template('simple_grid/fileserver.conf.erb')
   }
