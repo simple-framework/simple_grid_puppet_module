549482cfa936c84c20a9bac5d6a19f540a02cdc1
root <root@puppet-server.cern.ch>
2018-10-23 09:11:59 +0200

    Chaining functions for pre_conf.pp

--- manifests/pre_conf.pp
+++ manifests/pre_conf.pp
@@ -7,52 +7,69 @@ class simple_grid::pre_conf(
   $site_config_file
 ) {
   notify{"Running State: Pre Conf":}
-  #create config directory
+  # 1. create config directory
   file {"main config directory":
     ensure => directory,
     path   => "${config_dir}", 
     mode   => "0777",
   }
-  #create fileserver.conf
+  #2. create fileserver.conf
   file {"add fileserver.conf":
     ensure  => present,
     path    => "/etc/puppetlabs/puppet/fileserver.conf",
     content => template('simple_grid/fileserver.conf.erb')
   }
-  #check if site-level-config-file is present
+  #3. check if site-level-config-file is present
   file {"Check presence of site_config file":
     ensure => present,
     path   => "${site_config_dir}/${site_config_file}"
   }
-  #download simple grid yaml compiler
-  vcsrepo { "${config_dir}/${yaml_compiler_dir_name}":
+  #4. download simple grid yaml compiler
+  vcsrepo {"${config_dir}/${yaml_compiler_dir_name}":
     ensure   => present,
     provider => git,
     revision => $yaml_compiler_revision,
     source   => $yaml_compiler_repo_url,
+    before   => File["create temp directory for compiler"],
   }
-  # create virtual environment
-python::virtualenv { "virtual environment for yaml compiler": 
+  #5. create virtual environment
+  python::virtualenv { "virtual environment for yaml compiler": 
     ensure       => present,
     version      => 'system',
-    requirements => "${config_dir}/${yaml_compiler_dir_name}/requirements.txt",
+    #  requirements => "${config_dir}/${yaml_compiler_dir_name}/requirements.txt",
     systempkgs   => true,
     venv_dir     => "${config_dir}/${yaml_compiler_dir_name}/.env",
     cwd          => "${config_dir}/${yaml_compiler_dir_name}",
-  }
-  python::requirements {"${config_dir}/${yaml_compiler_dir_name}/requirements.txt":
+  }~>
+  #6. install required libs
+  python::pip {"pyyaml":
     virtualenv => "${config_dir}/${yaml_compiler_dir_name}/.env",
-  }
+  }~>
+  #6. install required libs
+  python::pip {"ruamel.ordereddict": 
+    virtualenv => "${config_dir}/${yaml_compiler_dir_name}/.env",
+  }~>
+  #6. install required libs
+  python::pip {"ruamel.yaml":
+    virtualenv => "${config_dir}/${yaml_compiler_dir_name}/.env",
+  }~>
+  #7. create tem directory
   file {"create temp directory for compiler":
-    ensure  => directory,
-    path    => "$config_dir/$yaml_compiler_dir_name/.temp",
-  }
+    ensure => directory,
+    path   => "$config_dir/$yaml_compiler_dir_name/.temp",
+    owner  => 'puppet',
+    mode   => '766',
+  }~>
+  #8. activate virtual environment
   exec {"source virtualenv":
     command => "bash -c 'source ${config_dir}/${yaml_compiler_dir_name}/.env/bin/activate'",
-    path    => "/usr/local/bin/:/usr/bin/:/bin/"
-  }
+    path    => "/usr/local/bin/:/usr/bin/:/bin/",
+    #before  => Exec["execute compiler"],
+  }~>
+  #9. execute the compiler
   exec {"execute compiler":
     command => "bash -c 'source ${config_dir}/${yaml_compiler_dir_name}/.env/bin/activate && python $config_dir/$yaml_compiler_dir_name/simple_grid_yaml_compiler.py $site_config_dir/$site_config_file  -o $config_dir/$yaml_compiler_dir_name/output.yaml'",
-    path    => '/usr/local/bin/:/usr/bin/:/bin/'
+    path    => '/usr/local/bin/:/usr/bin/:/bin/',
+    cwd     => "${config_dir}/${yaml_compiler_dir_name}",
     }
 }
