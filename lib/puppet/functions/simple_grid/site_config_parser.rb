e1caef0dd1dc60305091590f882c46d60c02b1fc
ejr004 <acessoej@icloud.com>
2018-11-21 15:13:43 +0100

    fix end

--- lib/puppet/functions/simple_grid/site_config_parser.rb
+++ lib/puppet/functions/simple_grid/site_config_parser.rb
@@ -20,7 +20,7 @@ Puppet::Functions.create_function(:'simple_grid::site_config_parser') do
                 if $query == 'site_infrastructure' then
                         data = YAML.load_file(site_level_config_file_path)
                         data['site_infrastructure']
-                else if $query == 'lightweight_components' then
+                else $query == 'lightweight_components' then
                         data = YAML.load_file(site_level_config_file_path)
                         data['lightweight_components']
                 end
