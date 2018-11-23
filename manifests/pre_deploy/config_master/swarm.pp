10c637abe323c1d9dd71112937c3f63c1ea88ac6
root <root@puppet-server.cern.ch>
2018-11-23 10:34:17 +0100

    Adding docker join

--- manifests/pre_deploy/config_master/swarm.pp
+++ manifests/pre_deploy/config_master/swarm.pp
@@ -1,20 +1,15 @@
 
 class simple_grid::pre_deploy::config_master::swarm(
   $site_level_config_dir = lookup('simple_grid::config::config_master::pre_deploy::site_level_config_dir'),
-  $site_level_config_file = lookup('simple_grid::config::config_master::pre_deploy::site_level_config_file')
-
+  $site_level_config_file = lookup('simple_grid::config::config_master::pre_deploy::site_level_config_file'),
 ){
   notify{"Running Stage: Docker Swarm":}
 
-# Get nodes hostname and ip address
 $output_site_infrastructure =  simple_grid::site_config_parser("$site_level_config_dir/$site_level_config_file","site_infrastructure")
 $output_lightweight_components =  simple_grid::site_config_parser("$site_level_config_dir/$site_level_config_file","lightweight_components")
 
 #notify {"result Site infrastructure: ${$output_site_infrastructure}":}
 #notify {"result Lightweight Components ${$output_lightweight_components}":}
-
-#notify{" The node is $output_lightweight_components['type']":}
-
 
  $output_lightweight_components.each |Integer $index, Hash $value| {
    if $value['type'] == 'compute_element' {
@@ -23,21 +18,25 @@ $output_lightweight_components =  simple_grid::site_config_parser("$site_level_c
            $node=$nodes['node']
            #notify{"AAAAAAAAAAAAAAA $node":}
             $output_site_infrastructure.each |Integer $index, Hash $value|{
-              if $value[hostname] == $node{
-                  $ip_ce = $value[ip_address]
-                  notify{"CE IP Address ${ip_ce}":}
-                  #exec{"swarm init for $ip":
-                  #    command => "bolt task run docker::swarm_init --node $ip",
-                  #    path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
-                  #    user    => 'root',
-                  #     }
-              } 
+                 if $value[hostname] == $node{
+                      $ip_ce = $value[ip_address]
+                      notify{"CE IP Address ${ip_ce}":}
+                       exec{"swarm init for $ip_ce":
+                            command => "bolt task run docker::swarm_init --node $ip_ce",
+                            path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
+                            user    => 'root',
+                      }
+                      exec{"generate swarm token on $ip_ce":
+                            command => "bolt task run docker::swarm_token node_role=worker --nodes $ip_ce > /tmp/$ip_ce.txt",
+                            path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
+                            user    => 'root',
+                      }
+                 } 
              }
-       }
+          }
      }
   }
-
- $output_lightweight_components.each |Integer $index, Hash $value| {
+  $output_lightweight_components.each |Integer $index, Hash $value| {
    if $value['type'] == 'worker_node' {
    #$node = $value['node']
       $value['nodes'].each |$key, $nodes|{
@@ -47,36 +46,20 @@ $output_lightweight_components =  simple_grid::site_config_parser("$site_level_c
               if $value[hostname] == $node{
                   $ip_wn = $value[ip_address]
                   notify{"WN IP Address ${ip_wn}":}
-                  #exec{"swarm init for $ip":
-                  #    command => "bolt task run docker::swarm_init --node $ip",
-                  #    path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
-                  #    user    => 'root',
-                  #     }
+                      exec{"swarm join for $ip":
+                      command => "bolt task run docker::swarm_init --node $ip_ce",
+                      path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
+                      user    => 'root',
+                       }
               }
              }
        }
      }
   }
-   #notify{"Index: $index value: $value":}
-       
-    # if $value['type'] == 'compute_element' {
-    #     notice("YOLO")
-    #  }
   
-  /*
-  $output_site_infrastructure.each |Integer $index, Hash $value| {
-      $ip = $value[ip_address]
-        notify{"IP address is ${ip}":}
-        exec{"swarm init for $ip":
-        command => "bolt task run docker::swarm_init --node $ip",
-        path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
-        user    => 'root',
-      }
-  }
-
 /*  
   
-  #swarm_token
+  #swarm_init
 bolt task show docker::swarm_init --nodes ce-simple 
 
 #swarm_token
