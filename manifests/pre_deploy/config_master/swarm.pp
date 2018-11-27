5ac28c336f450d737b152621cb02882ec57676d8
root <root@puppet-server.cern.ch>
2018-11-27 15:12:31 +0100

    simple_grid task for swarm

--- manifests/pre_deploy/config_master/swarm.pp
+++ manifests/pre_deploy/config_master/swarm.pp
@@ -4,77 +4,11 @@ class simple_grid::pre_deploy::config_master::swarm(
   $site_level_config_dir = lookup('simple_grid::config::config_master::pre_deploy::site_level_config_dir'),
   $site_level_config_file = lookup('simple_grid::config::config_master::pre_deploy::site_level_config_file'),
 ){
-  notify{'Running Stage: Docker Swarm':}
+notify{'Running Stage: Docker Swarm':}
 
-$output_site_infrastructure =  simple_grid::site_config_parser("${site_level_config_dir}/${site_level_config_file}",'site_infrastructure')
-$output_lightweight_components =  simple_grid::site_config_parser("${site_level_config_dir}/${site_level_config_file}",'lightweight_components')
-$token = simple_grid::token_extractor()
-
-#notify {"result Site infrastructure: ${$output_site_infrastructure}":}
-#notify {"result Lightweight Components ${$output_lightweight_components}":}
-
-$output_lightweight_components.each |Integer $index, Hash $value| {
-  if $value['type'] == 'compute_element' {
-  #$node = $value['node']
-    $value['nodes'].each |$key, $nodes|{
-      $node=$nodes['node']
-        #notify{"AAAAAAAAAAAAAAA $node":}
-        $output_site_infrastructure.each |Integer $index, Hash $value|{
-          if $value[hostname] == $node{
-            $ip_ce = $value[ip_address]
-            notify{"CE IP Address ${ip_ce}":}
-            exec{"swarm init for ${ip_ce}":
-                  command => "bolt task run docker::swarm_init --node ${ip_ce}",
+exec{"swarm init for ${ip_ce}":
+                  command => "bolt task run simple_grid::swarm --modulepath /etc/puppetlabs/code/environments/pre_deploy/site/ --nodes localhost",
                   path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
                   user    => 'root',
                   }
-            exec{"generate swarm token on ${ip_ce}":
-                  command => "bolt task run docker::swarm_token node_role=worker --nodes ${ip_ce} > /tmp/ce/${ip_ce}",
-                  path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
-                  user    => 'root',
-                  }
-               $token.each | $value|{
-                  notify{"Token: $value and ${ip_ce}":}
-                    }
-                
-                
-                }
-            }
-        }
-    }
-  }
 }
-/*
-$output_lightweight_components.each |Integer $index, Hash $value| {
-  if $value['type'] == 'worker_node' {
-  #$node = $value['node']
-    $value['nodes'].each |$key, $nodes|{
-      $node=$nodes['node']
-      #notify{"AAAAAAAAAAAAAAA $node":}
-      $output_site_infrastructure.each |Integer $index, Hash $value|{
-        if $value[hostname] == $node{
-          $ip_wn = $value[ip_address]
-          notify{"WN IP Address ${ip_wn}":}
-          exec{"swarm join for ${ip_wn}":
-              command => "bolt task run docker::swarm_init --node ${ip_wn}",
-              path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
-              user    => 'root',
-                }
-        }
-            }
-      }
-    }
-  }
-}
-#swarm_init
-#bolt task show docker::swarm_init --nodes ce-simple 
-
-#swarm_token
-#bolt task run docker::swarm_token node_role=worker --nodes ce-simple
-
-#swarm_join
-#bolt task run docker::swarm_join listen_addr=10.0.1.10  token=SWMTKN-1-28mzroiuuqlmw3xam8npb14wqyrh9er82qjmyw5rr1rccw9gzq-ba098vsi6w4vfmxen6kn5r53b manager_ip=188.184.29.186:2377 --nodes puppetclient
-
-#docker_network { 'simple':
-*/
-
--- tasks/swarm.rb
+++ tasks/swarm.rb
@@ -1,23 +1,13 @@
 #!/opt/puppetlabs/puppet/bin/ruby
 require 'yaml'
 require 'open3'
-#Puppet::Functions.create_function(:'simple_grid::site_config_parser') do
- #       dispatch :parse_param do
- #               param 'String', :site_level_config_file_path
-                #param 'String', :query
-#        end
-        #def parse_param(site_level_config_file_path, query)
-        #        data = YAML.load_file(site_level_config_file_path)
-        #        data['site_infrastructure']
-        #end
-        #def parse_param(site_level_config_file_path)
-        #        data = YAML.load_file(site_level_config_file_path)
-        #        data['site_infrastructure']
-        #end
-site_level_config_file_path = "/etc/simple_grid/simple_grid_site_config/site-level-configuration-file.yaml"
-hostname = Array.new
-ce_ip =  Array.new
+require 'json'
 
+site_level_config_file_path = "/etc/simple_grid/simple_grid_site_config/site-level-configuration-file.yaml"
+#hostname = Array.new
+#ce_ip =  Array.new
+
+# Get Hostname all hostnames
 def get_hostname(site_level_config_file_path)
         site_infrastructure = Hash.new
         hostname = Array.new
@@ -28,7 +18,7 @@ def get_hostname(site_level_config_file_path)
         end
         return hostname
 end
-
+# Get only CE ip Address
 def get_ce_ip(site_level_config_file_path)
         site_infrastructure = Hash.new
         lightweight_components = Hash.new
@@ -56,7 +46,7 @@ def get_ce_ip(site_level_config_file_path)
         end
         return ip_ce
 end
-
+# Get all ip addresses
 def get_element_ip(site_level_config_file_path, element)
         site_infrastructure = Hash.new
         lightweight_components = Hash.new
@@ -84,42 +74,55 @@ def get_element_ip(site_level_config_file_path, element)
         end
         return ip
 end
-
+# Init Swarm on CE and create NW simple
 def swarm_init(ce_ip)
         ce_ip.each do |ip|
-        puts  "***"
-        puts ip
+        puts  "** Initializing SWARM on #{ip} **"
         init_cmd = "bolt task run docker::swarm_init --node #{ip} --modulepath /etc/puppetlabs/code/environments/master/modules/"
+        puts  "***"
         system init_cmd
-        nw_cmd = "\'bolt command run docker network create --attachable --driver=overlay --subnet=10.0.1.0/24 simple\' --nodes #{ip}"
+        puts  "** Creating SIMPLE Network on #{ip} **"
+        nw_cmd = "bolt command run "+"'"+"docker network create --attachable --driver=overlay --subnet=10.0.1.0/24 simple"+ "'" + " --nodes  #{ip}"
+        puts  "***"
         system nw_cmd
         end
 end
-
+# Generate, extract token and join docker swarm
 def swarm_token(ce_ip,wn_ip)
         token = Array.new
         ce_ip.each do |cip|
-                puts  "Generating Token"
+                puts  "** Generating Token on #{cip} **"
+                puts  "***"
                 get_cmd = "bolt task run docker::swarm_token node_role=worker --nodes  #{cip} > /tmp/ce/#{cip}"
                 system get_cmd
-                puts  "Extracting Token"
+                puts  "** Extracting Token **"
+                puts  "***"
                 token = IO.readlines("/tmp/ce/#{cip}")[2]
                 token = token.delete(' ')
                 token = token.chop
                 puts token
+                puts  "***"
                 wn_ip.each do |wip|
-                        puts"######################"
+                        puts  "***"
+                        puts  "** Join Manager:#{cip} Worker:#{wip} with token:#{token} **"
                         join_cmd = "bolt task run docker::swarm_join listen_addr=10.0.1.10  token=#{token}  manager_ip=#{cip}:2377 --nodes #{wip}"
                         puts join_cmd
-                        puts"######################"
+                        puts  "***"
                         system join_cmd
                         end
         end
 end
 
-hostname = get_hostname(site_level_config_file_path)
+begin
+
+#hostname = get_hostname(site_level_config_file_path)
 ce_ip = get_element_ip(site_level_config_file_path,"compute_element")
 wn_ip = get_element_ip(site_level_config_file_path,"worker_node")
 
 swarm_init(ce_ip)
-#swarm_token(ce_ip,wn_ip) 
+swarm_token(ce_ip,wn_ip) 
+
+rescue Puppet::Error => e
+  puts({ status: 'failure', error: e.message }.to_json)
+  exit 1
+end
