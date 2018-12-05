013551ef7f90e5a187d21b0d8ffcef49c5724aa1
maany <imptodefeat@gmail.com>
2018-12-05 19:27:01 +0100

    #23 encapsulate simple_installer for config master in a puppet class

--- manifests/install/config_master/simple_installer.pp
+++ manifests/install/config_master/simple_installer.pp
@@ -1,24 +1,27 @@
-include 'simple_grid::ccm_function::create_config_dir'
+class simple_grid::install::config_master::simple_installer{
+  notify{"Creating simple config directory":}
+  include 'simple_grid::ccm_function::create_config_dir'
 
-notify{"Installing git":}
-package {"Install git":
-  name   => 'git',
-  ensure => present,
-}
+  notify{"Installing git":}
+  package {"Install git":
+    name   => 'git',
+    ensure => present,
+  }
 
-notify{"Installing External Node Classifier":}
-class {'simple_grid::components::enc::install':}
+  notify{"Installing External Node Classifier":}
+  class {'simple_grid::components::enc::install':}
 
-notify{"Configuring External Node Classifier":}
-class {'simple_grid::components::enc::configure':}
+  notify{"Configuring External Node Classifier":}
+  class {'simple_grid::components::enc::configure':}
 
-notify{"Creating a sample site level configuration file":}
-class {"simple_grid::components::site_level_config_file::install":}
+  notify{"Creating a sample site level configuration file":}
+  class {"simple_grid::components::site_level_config_file::install":}
 
-notify{"Installing Puppet CCM":}
-class {"simple_grid::components::ccm::install":}
+  notify{"Installing Puppet CCM":}
+  class {"simple_grid::components::ccm::install":}
 
-notify{"Configuring CCM on Config Master":}
-class{"simple_grid::components::ccm::config":
-  node_type => "CM"
+  notify{"Configuring CCM on Config Master":}
+  class{"simple_grid::components::ccm::config":
+    node_type => "CM"
+  }
 }
