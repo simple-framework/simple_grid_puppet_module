7744de74530d01cdfbedcfa4efc5c8d60d2d068e
maany <imptodefeat@gmail.com>
2018-12-12 01:05:30 +0100

    #37 fix deserialization from facts

--- lib/puppet/functions/simple_grid/puppet_conf_from_fact.rb
+++ lib/puppet/functions/simple_grid/puppet_conf_from_fact.rb
@@ -5,15 +5,16 @@ Puppet::Functions.create_function(:'simple_grid::puppet_conf_from_fact') do
     def puppet_conf_from_fact(puppetfile_content)
         _output_array = puppetfile_content.split(";")
         _data = Hash.new
+        _section_key = String.new
         _output_array.each do |line|
             if line.include? "["
-                section_key = line.strip
-                _data.update(section_key => {})
-            # elsif line.include? "="
-            #     split_data = line.split('=')
-            #     key = split_data[0].strip
-            #     value = split_data[1].strip
-            #     _data[section_key].update(key => value)
+                _section_key = line.strip
+                _data.update(_section_key => {})
+            elsif line.include? "="
+                split_data = line.split('=')
+                key = split_data[0].strip
+                value = split_data[1].strip
+                _data[_section_key].update(key => value)
             end
         end
         _data
