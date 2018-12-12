f69b8efbfb66c272b115727b471befd9404a46ee
maany <imptodefeat@gmail.com>
2018-12-12 19:44:51 +0100

    #38 #39 update init.pp to install stage

--- manifests/install/lightweight_component/init.pp
+++ manifests/install/lightweight_component/init.pp
@@ -1,4 +1,4 @@
-class simple_grid::pre_config::lightweight_component::init{
+class simple_grid::install::lightweight_component::init{
   # Class['simple_grid::pre_config::lightweight_component::ssh_config'] -> Class['simple_grid::pre_config::lightweight_component::reset_agent']
   # class { 'simple_grid::pre_config::lightweight_component::ssh_config': }
   # class {'simple_grid::pre_config::lightweight_component::reset_agent':}
