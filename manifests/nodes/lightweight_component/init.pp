8182721d4a35a6ffac3b86a615a1303b4e32ad59
maany <imptodefeat@gmail.com>
2018-12-13 16:00:41 +0100

    Stage management instructions -2

--- manifests/nodes/lightweight_component/init.pp
+++ manifests/nodes/lightweight_component/init.pp
@@ -9,13 +9,13 @@ class simple_grid::nodes::lightweight_component::init
     #start container here for the first entry in $facts['execution_pending']
   }
   elsif $simple_stage == lookup('simple_grid::stage::final'){
-    #for each execution id,
+    #for each execution id on this LC node, make sure firewall rules are correct, containers are running, perform any tests to check the status of containers.
   }
   elsif $simple_stage == lookup('simple_grid::stage::install'){
     class{"simple_grid::install::lightweight_component::init":}    
     class{"simple_grid::config::lightweight_component_init":} #not in specification, added to do puppet specific configuration
     class {"simple_grid::components::execution_stage_manager::set_stage":
-      simple_stage => lookup('simple_grid::stage::pre_deploy') #hadnled by tasks executed by CM
+      simple_stage => lookup('simple_grid::stage::pre_deploy') #handled by tasks executed by CM
     }
   }
 }
