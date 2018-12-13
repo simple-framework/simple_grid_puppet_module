fbabd2f7bb90cba74cee946ff2b46483c421e45f
maany <imptodefeat@gmail.com>
2018-12-13 17:24:03 +0100

    fix syntax errors

--- manifests/pre_deploy/config_master/init.pp
+++ manifests/pre_deploy/config_master/init.pp
@@ -10,7 +10,7 @@ class simple_grid::pre_deploy::config_master::init(
       user    => 'root',
     }
   }
-  elsif $mode == lookup('simple_grid::mode::release' {
+  elsif $mode == lookup('simple_grid::mode::release') {
     exec{"Set up docker swarm on the entire cluster":
       command => "bolt task run simple_grid::swarm augmented_site_level_config_file='/etc/simple_grid/site_config/augmented_site_level_configuration_file.yaml' modulepath='/etc/puppetlabs/code/environments/simple/modules' --nodes localhost",
       path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
