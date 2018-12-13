251c5b07a64003aae6b6b088885bcf11cc83396c
maany <imptodefeat@gmail.com>
2018-12-13 14:54:49 +0100

    refactor code for pre_deploy stage on LC

--- manifests/config/lightweight_component/lightweight_component.pp
+++ manifests/config/lightweight_component/lightweight_component.pp
@@ -1,17 +1,1 @@
-class simple_grid::lightweight_component(
-  $site_config_dir = lookup("simple_grid::simple_config_dir"),
-  $site_config_file = lookup("simple_grid::config::config_master::pre_conf::site_config_file")
-){
-  file { "/etc/simple_grid":
-    ensure => directory,
-    mode   => "0644",
-    owner  => "puppet",
-  }
-   file { "copy site level config file to agent":
-        mode   => "0644",
-        owner  => "puppet",
-        source => 'puppet:///simple_grid/${site_config_dir}/${site_config_file}',
-        path   => "/etc/simple_grid/simple_grid_site_config_blah" 
-    }
 
-}
--- manifests/nodes/lightweight_component/init.pp
+++ manifests/nodes/lightweight_component/init.pp
@@ -1,28 +1,15 @@
-class simple_grid::nodes::lightweight_component::init(
-  $augmented_site_level_config_file = lookup('simple_grid::components::yaml_compiler::output')
-){
+class simple_grid::nodes::lightweight_component::init
+{
+  if $simple_stage == lookup('simple_grid::stage::pre_deploy') {
 
-  if $simple_stage == lookup('simple_grid::stage::install') {
-    notify{"**** Node LC; Stage Pre_Deploy":}
-    $deploy_status_file = lookup('simple_grid::nodes::lightweight_component::deploy_status_file')
-    $content = loadyaml("${augmented_site_level_config_file}")
-    $execution_ids = simple_grid::generate_execution_ids($content, $fqdn)
-    notify{"Execution id for $fqdn are $execution_ids":}
-    $deploy_status = {
-      "execution_pending"    => $execution_ids,
-      "execution_completed"  => []
-    }
-    file{"Write execution ID's to ${deploy_status_file}":
-      path    => "${deploy_status_file}",
-      content => to_yaml($deploy_status),
-      ensure  => present,
-    }
-  }else{
-    class {"simple_grid::components::ccm::config":
-      node_type => "LC"
-    }
+  }
+  elsif $simple_stage == lookup('simple_grid::stage::final'){
+
+  }
+  elsif $simple_stage == lookup('simple_grid::stage::install'){
+    class{"simple_grid::pre_deploy::lightweight_component::init":}    
     class {"simple_grid::components::execution_stage_manager::set_stage":
-      simple_stage => lookup('simple_grid::stage::install')
+     simple_stage => lookup('simple_grid::stage::pre_deploy')
     }
   }
 }
