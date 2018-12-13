44b9723e17a77afeaca55fa4726dcdba71c7609f
maany <imptodefeat@gmail.com>
2018-12-13 23:16:14 +0100

    cleanup swarm tasks (get_element_ip)

--- tasks/swarm.rb
+++ tasks/swarm.rb
@@ -7,34 +7,21 @@ require 'json'
 
 # Get all ip addresses
 def get_element_ip(site_level_config_file_path, element)
-    site_infrastructure = Hash.new
-    lightweight_components = Hash.new
-    type = Hash.new
-    hostname = Array.new
-    nodes =Array.new
-    node =Array.new
     ip = Array.new
     data = YAML::load_file(site_level_config_file_path)
     site_infrastructure = data["site_infrastructure"]
     lightweight_components = data["lightweight_components"]
-    #puts(site_infrastructure)
     lightweight_components.each do |lc|
         if lc["type"] == element then
-            nodes << lc["deploy"]
-            nodes.each do |node_array|
-                #puts node_array
-                node_array.each do |value|
-                     puts value
-                        #site_infrastructure.each do |site_infra|
-                     #   puts value[0]
-                         #if site_infra["fqdn"] ==  value["node"]
-                                 #ip << site_infra["ip_address"]
-                         #end
-                     #end
-                 end
+            site_infrastructure.each do |node|
+                if node['fqdn'] == lc['deploy']['node']
+                    ip << node["ip_address"]
+                end
             end
         end
     end
+    puts "YOLO"
+    puts ip
     return ip
 end
 # Init Swarm on CE and create NW simple
