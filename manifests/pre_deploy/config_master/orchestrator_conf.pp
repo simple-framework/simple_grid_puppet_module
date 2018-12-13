c72f796d93597601ecce5cfbe1cd1a7935348652
maany <imptodefeat@gmail.com>
2018-12-13 16:21:41 +0100

    remove deprecated files

--- manifests/pre_deploy/config_master/orchestrator_conf.pp
+++
@@ -1,4 +0,0 @@
-class simple_grid::pre_deploy::config_master::orchestrator_conf {
-  notify{"Running Stage: Orchestrator Conf":}
-  class{"simple_grid::pre_deploy::config_master::swarm":}
-}
--- manifests/pre_deploy/config_master/pre_orchestration.pp
+++
@@ -1,5 +0,0 @@
-class simple_grid::pre_deploy::config_master::pre_orchestration{
-  notify{"Running Stage: Pre-Orchestrator Conf":}
-  class{"simple_grid::config::config_master::swarm":
-  }
-}
