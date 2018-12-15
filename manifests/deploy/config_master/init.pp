f4e7a792cc72b7e00479516a051cbb7fc455c771
maany <imptodefeat@gmail.com>
2018-12-15 19:26:46 +0100

    #27 add wait_for dependency

--- manifests/deploy/config_master/init.pp
+++ manifests/deploy/config_master/init.pp
@@ -1,3 +1,15 @@
-class simple_grid::deploy::config_master::init(){
-  
+class simple_grid::deploy::config_master::init(
+  $augmented_site_level_config_file = lookup('simple_grid::components::yaml_compiler::output')
+){
+  $augmented_site_level_config = loadyaml("${augmented_site_level_config_file}")
+  $lightweight_components = $augmented_site_level_config['lightweight_components']
+  $lightweight_components.each |Integer $index, Hash $lightweight_component| {
+    notify{"Executing ${index} for ${lightweight_component['name']}":}
+    #wait for previous node to be completely execution
+    #task > /tmp/status
+    #if status is completed
+      #execution for the current node
+    #if not completed, try again 
+  }
+
 }
--- manifests/nodes/config_master/init.pp
+++ manifests/nodes/config_master/init.pp
@@ -2,12 +2,13 @@ class simple_grid::nodes::config_master::init{
   if $simple_stage == lookup('simple_grid::stage::pre_deploy'){
     # aggregate lifecycle scripts on CM, set up container orchestrator, change LC stage to deploy, reset agent to 30m
     include simple_grid::pre_deploy::config_master::init
-    #class {"simple_grid::components::execution_stage_manager::set_stage":
-    #  simple_stage => lookup('simple_grid::stage::pre_deploy')
-    #}
+    class {"simple_grid::components::execution_stage_manager::set_stage":
+     simple_stage => lookup('simple_grid::stage::deploy')
+    }
   }
   elsif $simple_stage == lookup('simple_grid::stage::deploy'){
     #deployment for CM, run 'THE LOOP' here
     #for each entry in loop, do a task to execute puppet apply -e 'class{"simple_grid::deploy::lightweight_component::init": execution_id => "{value from loop}"}
+    include simple_grid::deploy::config_master::init
   }
 }
