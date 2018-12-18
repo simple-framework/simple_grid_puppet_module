20400bc3a8a319c395236ee923f382ac0cc75f6e
maany <imptodefeat@gmail.com>
2018-12-18 18:55:56 +0100

    #27 #50 setup logging on LC's at /etc/simple_grid/.deploy.log

--- tasks/deploy.rb
+++ tasks/deploy.rb
@@ -5,7 +5,7 @@ require 'open3'
 require 'puppet'
 
 def init_deploy(execution_id)
-    puppet_apply = "puppet apply -e \"class{'simple_grid::deploy::lightweight_component::init':execution_id =>#{execution_id}}\""
+    puppet_apply = "puppet apply -e \"class{'simple_grid::deploy::lightweight_component::init':execution_id =>#{execution_id}}\" > /etc/simple_grid/.deploy.log"
     stdout, stderr, status = Open3.capture3(puppet_apply)
     raise Puppet::Error, ("stderr: '#{stderr}'") if status !=0
     puts "Output",stdout
