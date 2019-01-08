68b968ffc4b11cc07e6c0877a33a945b59465d44
maany <imptodefeat@gmail.com>
2019-01-08 15:39:11 +0100

    #27 #51 fix facts execution_pending and execution_completed

--- lib/facter/execution_pending.rb
+++ lib/facter/execution_pending.rb
@@ -1,16 +1,16 @@
 Facter.add('execution_pending') do
     setcode do
       begin
-        data = YAML.load(File.read("/etc/simple_grid/.deploy_status.yaml"))
-        exeuction_pending = []
-        data.each do |deploy_status|
+        data = YAML.load(File.read("/etc/simple_grid/.deploy_status.yaml")) #try lookup
+        execution_pending = []
+        data['deploy_status'].each do |deploy_status|
           if deploy_status['status'] == 'pending'
             execution_pending << deploy_status['execution_id']
           end
         end
         execution_pending.join(',')
       rescue Exception => ex
-        "uninitialized"
+        ex.message
       end
     end
   end
