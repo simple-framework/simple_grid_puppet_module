25e2b780325dc87e810cdc36cfa3b59d3daa8c00
maany <imptodefeat@gmail.com>
2019-01-11 17:12:30 +0100

    #27 append a  dummy LC

--- manifests/deploy/config_master/init.pp
+++ manifests/deploy/config_master/init.pp
@@ -6,9 +6,15 @@ class simple_grid::deploy::config_master::init(
 ){
   $augmented_site_level_config = loadyaml("${augmented_site_level_config_file}")
   $lightweight_components = $augmented_site_level_config['lightweight_components']
-
-  $lightweight_components.each |Integer $index, Hash $lightweight_component| {
-    if $index > 0 {
+  # Append a dummy lightweight_component in the end.
+  $dummy_final_lightweight_component = {
+    "name" => "Dummy LC", 
+    "description" => "Required to ensure processing of return codes of the final lightweight_component"
+  }
+  $lightweight_components_augmented = concat($lightweight_components, $dummy_final_lightweight_component)
+  $final_index = length($lightweight_components_augmented) - 1
+  $lightweight_components_augmented.each |Integer $index, Hash $lightweight_component| {
+    if $index > 0 { 
       $last_index = $index -1 
       $execution_status = file("/etc/simple_grid/.${last_index}.status")
       notify{"Execution Status of ${last_index} was ${execution_status}":}
@@ -16,36 +22,38 @@ class simple_grid::deploy::config_master::init(
         fail("Error Message will be shown here")
       }
     }
-    $node_fqdn = $lightweight_component['deploy']['node']
-    notify{"Deploying ${lightweight_component['name']} on ${node_fqdn} with execution_id ${index}":}
-    exec{"Executing puppet agent on ${node_fqdn} to deploy ${lightweight_component['name']} with execution_id ${index}":
-     command => "bolt task run simple_grid::deploy execution_id=${index} --modulepath /etc/puppetlabs/code/environments/simple/site/ --nodes ${node_fqdn}",
-     path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
-     user    => 'root',
-     logoutput => 'on_failure',
-     environment => ["HOME=/root"]
+    #if index is last element (dummy component, exit here), else do the following
+    if( $index == $final_index){
+        notify{"*** Successfully reached end of Deployment Stage ****":}
     }
-  
-    exec{"Waiting for deployment of ${index} to end":
-      command => "bolt task run simple_grid::deploy_status \
-      node_fqdn=${node_fqdn} \
-      deploy_status_file=${deploy_status_file} \
-      execution_id=${index} retry_interval=${retry_interval} \
-      max_retries=${max_retries} \
-      modulepath= /etc/puppetlabs/code/environments/production/modules/ \
-      --modulepath /etc/puppetlabs/code/environments/simple/site/:/etc/puppetlabs/code/environments/simple/modules/ \
-      --nodes localhost > /etc/simple_grid/.${index}.status", #${node_fqdn} > /etc/simple_grid/${index}.status",
+    else {
+      $node_fqdn = $lightweight_component['deploy']['node']
+      notify{"Deploying ${lightweight_component['name']} on ${node_fqdn} with execution_id ${index}":}
+      exec{"Executing puppet agent on ${node_fqdn} to deploy ${lightweight_component['name']} with execution_id ${index}":
+      command => "bolt task run simple_grid::deploy execution_id=${index} --modulepath /etc/puppetlabs/code/environments/simple/site/ --nodes ${node_fqdn}",
       path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
-      user    => root,
+      user    => 'root',
       logoutput => 'on_failure',
       environment => ["HOME=/root"]
+      }
+    
+      exec{"Waiting for deployment of ${index} to end":
+        command => "bolt task run simple_grid::deploy_status \
+        node_fqdn=${node_fqdn} \
+        deploy_status_file=${deploy_status_file} \
+        execution_id=${index} retry_interval=${retry_interval} \
+        max_retries=${max_retries} \
+        modulepath= /etc/puppetlabs/code/environments/production/modules/ \
+        --modulepath /etc/puppetlabs/code/environments/simple/site/:/etc/puppetlabs/code/environments/simple/modules/ \
+        --nodes localhost > /etc/simple_grid/.${index}.status", #${node_fqdn} > /etc/simple_grid/${index}.status",
+        path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
+        user    => root,
+        logoutput => 'on_failure',
+        environment => ["HOME=/root"]
+      }
+      exec{"chak de phattay $index":
+        command => "/init.sh"
+      }
     }
-  
-    #$status_file = file("etc/simple_grid/.${index}.status")
-    # exec{"Processing deployment result for ${index}":
-    #   command => "bolt task run simple_grid::deploy_status execution_id=${index} --modulepath /etc/puppetlabs/code/environments/simple/site/:/etc/puppetlabs/code/environments/simple/modules/ --nodes localhost >> /etc/simple_grid/.${index}.status", #${node_fqdn} > /etc/simple_grid/${index}.status",
-    #   path    => '/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin/',
-    #   user    => root
-    # }
   }
 }
