64087173ffddf81b0bd90652d9dd35f8f274b145
maany <imptodefeat@gmail.com>
2019-02-17 23:15:42 +0100

    #65 copy host certificates

---
+++ manifests/pre_deploy/lightweight_component/copy_host_certificates.pp
@@ -0,0 +1,19 @@
+class simple_grid::pre_deploy::lightweight_component::copy_host_certificates(
+  $host_certificates_dir = lookup('simple_grid::host_certificates_dir'),
+  $host_certificates_dir_name = lookup('simple_grid::host_certificates_dir_name'),
+){
+  $copy_host_certificates = simple_grid::check_presence_host_certificates($host_certificates_dir, $fqdn)
+  if $copy_host_certificates {
+    notify{'Copying host certificates for ${fqdn}'}
+    file{"Creating directory ${host_certificates_dir}":
+      ensure => directory,
+      path   => "${host_certificates_dir}",
+    }
+    file{"Copying host copy_host_certificates from CM to ${host_certificates_dir}":
+      ensure => directory,
+      source => "puppet:///simple_grid/${host_certificates_dir_name}/${fqdn}",
+      path => "${host_certificates_dir}",
+      mode => "0644",
+    }  
+  }
+}
