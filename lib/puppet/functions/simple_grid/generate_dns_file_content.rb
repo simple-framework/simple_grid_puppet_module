264f23f8ecce620dfee2878276f2a302ac925369
maany <imptodefeat@gmail.com>
2019-02-17 20:01:25 +0100

    #60 generate dns info and append to augmented site level config

--- lib/puppet/functions/simple_grid/generate_dns_file_content.rb
+++ lib/puppet/functions/simple_grid/generate_dns_file_content.rb
@@ -8,20 +8,23 @@ Puppet::Functions.create_function(:'simple_grid::generate_dns_file_content') do
     end
     def generate_dns_file_content(augmented_site_level_config_file, subnet, meta_info_prefix)
         dns_content = Array.new
-        
         net = IPAddr.new subnet
         ip_range = net.to_range.to_a
         ip_offset = 1
-        container_names = Array.new
+        ip_index = 1
         data = YAML.load_file(augmented_site_level_config_file)
         lightweight_components = data['lightweight_components']
         lightweight_components.each do |lightweight_component|
             name = lightweight_component['name']
             meta_info_parent = generate_meta_info_parent_name(meta_info_prefix,name)
             meta_info = data[meta_info_parent]
-            container_name = ""
+            container_type = meta_info['type']
+            container_name = Array.new
             if meta_info['docker_run_parameters'].key?("hostname")
-                container_names << meta_info['docker_run_parameters']['hostname'] 
+                container_name = meta_info['docker_run_parameters']['hostname'] 
+                ip_address = ip_range[ip_index]
+                ip_index=ip_index+1
+                dns_content << {"hostname" => "#{container_name}", 'ip' => ip_address.to_s, 'type' => container_type}
             else
                 execution_id = lightweight_component['execution_id']
                 fqdn = lightweight_component['deploy']['node'].split('.')
@@ -29,15 +32,14 @@ Puppet::Functions.create_function(:'simple_grid::generate_dns_file_content') do
                 domain = fqdn[1, fqdn.length].join('.')
                 container_count = lightweight_component['deploy']['container_count'].to_i
                 for index in 0..container_count
-                    container_names << [name, index, hostname, execution_id, domain].join("_")
+                    container_name = [name, index, hostname, execution_id].join("_") + ".#{domain}"
+                    ip_address = ip_range[ip_index]
+                    ip_index=ip_index+1
+                    dns_content << {"hostname" => "#{container_name}", 'ip' => ip_address.to_s, 'type' => container_type}
                 end
             end
         end
-        container_names.each_with_index do |container_name, index|
-            ip_address = ip_range[index+1]
-            dns_content << {"#{container_name}" => ip_address.to_s}
-        end
-        dns_content.to_yaml
+        dns_content.to_yaml.lines.to_a[1..-1].join
     end
 
     def generate_meta_info_parent_name(meta_info_prefix, component_name)
--- manifests/ccm_function/exec_repository_lifecycle_event.pp
+++ manifests/ccm_function/exec_repository_lifecycle_event.pp
@@ -1,3 +1,28 @@
-class simple_grid::ccm_function::exec_repository_lifeycle_event(){
-  
+class simple_grid::ccm_function::exec_repository_lifeycle_event(
+  $event,
+  $current_lightweight_element,
+  $execution_id,
+  $component_repository_dir = lookup('simple_grid::nodes::lightweight_component::component_repository_dir'),
+  $pre_config_script = lookup('')
+){
+  $repository_name = current_lightweight_element['name']
+  $repository_path = "${component_repository_dir}/${repository_name}"
+  $repository_pre_conf_script = "${repository_path}/${pre_config_script}"
+  if $mode == lookup('simple_grid::mode::docker') or $mode == lookup('simple_grid::mode::dev') {
+      exec{"Executing Pre-Config Script $script":
+        command => "${repository_pre_conf_script}",
+        path => '/usr/sue/sbin:/usr/sue/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin',
+        user => 'root',
+        logoutput => true,
+        environment => ["HOME=/root"]
+      }
+    }
+    elsif $mode == lookup('simple_grid::mode::release') {
+      exec{"Executing Pre-Config Script $script":
+        command => "${repository_pre_conf_script}",
+        path => '/usr/sue/sbin:/usr/sue/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/opt/puppetlabs/bin',
+        user => 'root',
+        logoutput => true
+      }
+    }
 }
