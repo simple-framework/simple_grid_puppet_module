701f4ee7a9fcc6627045ecffbd0e355b059f5172
maany <imptodefeat@gmail.com>
2019-02-27 14:53:39 +0100

    #27 docker run test

--- lib/puppet/functions/simple_grid/docker_run.rb
+++ lib/puppet/functions/simple_grid/docker_run.rb
@@ -2,9 +2,10 @@ Puppet::Functions.create_function(:'simple_grid::docker_run') do
     dispatch :docker_run do
         param 'Hash', :augmented_site_level_config
         param 'Hash', :current_lightweight_component
+        param 'String', :config_dir
     end
-    def docker_run(augmented_site_level_config, current_lightweight_component)
-        docker_run = "docker run "
+    def docker_run(augmented_site_level_config, current_lightweight_component, config_dir)
+        docker_run = "docker run -v #{config_dir}:/config"
         return docker_run
     end
 end
