776af7d80f48a7b268e37983cb4bf02f34e163cd
maany <imptodefeat@gmail.com>
2019-03-18 17:53:03 +0100

    #27 wire up docker_run.rb

--- lib/puppet/functions/simple_grid/docker_run.rb
+++ lib/puppet/functions/simple_grid/docker_run.rb
@@ -2,10 +2,19 @@ Puppet::Functions.create_function(:'simple_grid::docker_run') do
     dispatch :docker_run do
         param 'Hash', :augmented_site_level_config
         param 'Hash', :current_lightweight_component
+        param 'Hash', :meta_info
         param 'String', :config_dir
     end
-    def docker_run(augmented_site_level_config, current_lightweight_component, config_dir)
-        docker_run = "docker run -v #{config_dir}:/config"
-        return docker_run
+    def docker_run(augmented_site_level_config, current_lightweight_component, meta_info, config_dir)
+        docker_run_parameters = meta_info['docker_run_parameters']
+        
+        #name
+        # volume mounts
+        docker_run = "docker run" << " "
+        docker_run << "-v #{config_dir}:/config" << " "
+        
+        #cvmfs
+        #if meta_info.key?("cvmfs") and meta_info['cvmfs'] == true
+        docker_run
     end
 end
--- manifests/components/component_repository.pp
+++ manifests/components/component_repository.pp
@@ -18,21 +18,23 @@ class simple_grid::components::component_repository::deploy(
   #   meta_info                     => $meta_info
   # }
 
-  class{"simple_grid::ccm_function::exec_repository_lifecycle_hook":
-    hook => lookup('simple_grid::components::component_repository::lifecycle::hook::pre_config'),
-    current_lightweight_component => $current_lightweight_component,
-    execution_id => $execution_id
-  }
+  # class{"simple_grid::ccm_function::exec_repository_lifecycle_hook":
+  #   hook => lookup('simple_grid::components::component_repository::lifecycle::hook::pre_config'),
+  #   current_lightweight_component => $current_lightweight_component,
+  #   execution_id => $execution_id
+  # }
 
-  class{"simple_grid::ccm_function::exec_repository_lifecycle_event":
-    event => lookup('simple_grid::components::component_repository::lifecycle::event::pre_config'),
-    current_lightweight_component => $current_lightweight_component,
-    execution_id => $execution_id
-  }
-  class{"simple_grid::ccm_functoin::exec_repository::lifecycle_event":
+  # simple_grid::ccm_function::exec_repository_lifecycle_event{'Pre_config Event':#class{"simple_grid::ccm_function::exec_repository_lifecycle_event":
+  #   event => lookup('simple_grid::components::component_repository::lifecycle::event::pre_config'),
+  #   current_lightweight_component => $current_lightweight_component,
+  #   execution_id => $execution_id,
+  #   meta_info => $meta_info
+  # }
+  simple_grid::ccm_function::exec_repository_lifecycle_event{'Boot Event':#class{"simple_grid::ccm_function::exec_repository_lifecycle_event":
     event => lookup('simple_grid::components::component_repository::lifecycle::event::boot'),
     current_lightweight_component => $current_lightweight_component,
-    execution_id => $execution_id
+    execution_id => $execution_id,
+    meta_info => $meta_info
   }
 
 }
@@ -102,17 +104,26 @@ class simple_grid::component::component_repository::lifecycle::event::pre_config
 }
 class simple_grid::component::component_repository::lifecycle::event::boot(
   $current_lightweight_component,
+  $component_repository_dir = lookup('simple_grid::nodes::lightweight_component::component_repository_dir'),
+  $augmented_site_level_config_file = lookup('simple_grid::components::yaml_compiler::output'),
   $execution_id,
+  $meta_info,
 ){
   $augmented_site_level_config = loadyaml($augmented_site_level_config_file)
-  $docker_run_command = simple_grid::docker_run($augmented_site_level_config, $current_lightweight_component)
-  exec{"Booting container for ${current_lightweight_component['name']}":
-    command => $docker_run_command,
-    path => "/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin",
-    user => "root",
-    logoutput => true,
-    environment => ["HOME=/root"]
-  }
+  $level_2_configurator = simple_grid::get_level_2_configurator($augmented_site_level_config, $current_lightweight_component)
+  $repository_name = $current_lightweight_component['name']
+  $repository_path = "${component_repository_dir}/${repository_name}"
+  $config_dir = "${repository_path}/config"
+  $docker_run_command = simple_grid::docker_run($augmented_site_level_config, $current_lightweight_component, $meta_info, $config_dir)
+  notify{"Docker run command":}
+  notify{"${docker_run_command}":}
+  # exec{"Booting container for ${current_lightweight_component['name']}":
+  #   command => $docker_run_command,
+  #   path => "/usr/local/bin/:/usr/bin/:/bin/:/opt/puppetlabs/bin",
+  #   user => "root",
+  #   logoutput => true,
+  #   environment => ["HOME=/root"]
+  # }
 }
 class simple_grid::component::component_repository::lifecycle::hook::pre_init(
   $current_lightweight_component,
