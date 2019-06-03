09f7e107c429c6897f5192cb7916c9b66ddfe350
root <root@cm-01.cern.ch>
2019-06-03 12:14:22 +0200

    issue 53 Install bolt package and fix create dir recursively

--- manifests/components/bolt.pp
+++ manifests/components/bolt.pp
@@ -2,15 +2,18 @@ class simple_grid::components::bolt::install (
   $bolt_dir= lookup('simple_grid::components::bolt::bolt_dir')
 )
 {
-  notify {"Creating bolt directory":}
 
-  file {"/root/.puppetlabs/":
-    # path    => "/root/.puppetlabs/bolt/",
-    path   => "${$bolt_dir}",
+  notify {"Installing bolt package":}
+  package {"bolt":
+    ensure => installed,
+   }
+
+  notify {"Creating bolt directory":}
+  # Creating dir recursively 
+  file {["/root/","/root/.puppetlabs/","/root/.puppetlabs/bolt"]:
     ensure => directory,
   } ~>
   file { "Bolt config file from template":
-    # path    => "/root/.puppetlabs/bolt/bolt.yaml",
     path    => "${$bolt_dir}/bolt.yaml",
     ensure  => present,
     content => epp('simple_grid/bolt.yaml')
