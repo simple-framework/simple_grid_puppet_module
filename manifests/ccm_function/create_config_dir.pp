c27f0c3a73013964bd15a41b650d7f43df648b68
maany <imptodefeat@gmail.com>
2019-07-24 15:26:02 +0200

    #96 add log dir hiera param

--- manifests/ccm_function/create_config_dir.pp
+++ manifests/ccm_function/create_config_dir.pp
@@ -1,9 +1,16 @@
 class simple_grid::ccm_function::create_config_dir(
-  $simple_config_dir = lookup('simple_grid::simple_config_dir')
+  $simple_config_dir = lookup('simple_grid::simple_config_dir'),
+  $simple_log_dir    = lookup('simple_grid::simple_log_dir')
 ){
   file{"ccm_function_create_config_dir at $simple_config_dir":
     ensure => directory,
     mode   => "0777",
     path   => "$simple_config_dir"
+  }
+  file{"ccm_function_create_log_dir":
+    ensure => directory,
+    recurse => true,
+    mode   => "0777",
+    path   => "$simple_log_dir"
   }
 }
