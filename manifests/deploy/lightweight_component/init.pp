b404de7e744b66d7ac06c70fa7d3d4975c2d1f5f
maany <imptodefeat@gmail.com>
2019-07-24 14:46:08 +0200

    #96 move deploy status to /log/deploy_status

--- manifests/deploy/lightweight_component/init.pp
+++ manifests/deploy/lightweight_component/init.pp
@@ -10,7 +10,7 @@
     # $execute_now = simple_grid::execute_now($deploy_status_file, $execution_id, $intial_deploy_status)
     # notify{"Execute Now? ${execute_now}":}
     # if $execute_now {
-        # simple_grid::set_execution_status('/etc/simple_grid/.deploy_status.yaml', $execution_id, "deploying")
+        # simple_grid::set_execution_status('/etc/simple_grid/logs/deploy_status.yaml', $execution_id, "deploying")
         if $deploy_step == lookup('simple_grid::stage::deploy::step_1') {
             class {'simple_grid::components::component_repository::deploy_step_1':
                 execution_id => $execution_id
