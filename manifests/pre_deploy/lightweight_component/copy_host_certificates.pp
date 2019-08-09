d9cc665d70213e90d88e6d42617bd263697b88fe
maany <imptodefeat@gmail.com>
2019-08-09 04:02:29 +0200

    #101

--- manifests/pre_deploy/lightweight_component/copy_host_certificates.pp
+++ manifests/pre_deploy/lightweight_component/copy_host_certificates.pp
@@ -5,13 +5,25 @@ class simple_grid::pre_deploy::lightweight_component::copy_host_certificates(
   $copy_host_certificates = simple_grid::check_presence_host_certificates($host_certificates_dir, $fqdn)
   notify{"Copy host certificates? ${copy_host_certificates}":}
   if $copy_host_certificates {
-    notify{'Copying host certificates for ${fqdn}':}
+    notify{"Creating host certificates dir for ${fqdn}":}
     file{"Copying host copy_host_certificates from CM to ${host_certificates_dir}":
       ensure => directory,
       recurse => 'remote',
       source => "puppet:///simple_grid/${host_certificates_dir_name}/${fqdn}",
       path => "${host_certificates_dir}",
       mode => "0644",
-    }  
+    }
+    notify{"Setting permissiions for hostkey.pem":}
+    file{"${host_certificates_dir}/hostkey.pem":
+      ensure => present,
+      path => "${host_certificates_dir}/hostkey.pem",
+      mode => "0400",
+    }
+    notify{"Setting permissions for hostcert.pem}":}
+    file{"${host_certificates_dir}/hostcert.pem":
+      ensure => present,
+      path => "${host_certificates_dir}/hostcert.pem",
+      mode => "0444",
+    }
   }
 }
