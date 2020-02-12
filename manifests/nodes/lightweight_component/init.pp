144837954b15664dd228a2b8d3a5f841336efb0a
maany <imptodefeat@gmail.com>
2020-02-13 00:26:22 +0100

    #168 create wrapper script and copy it during pre_deploy

--- manifests/nodes/lightweight_component/init.pp
+++ manifests/nodes/lightweight_component/init.pp
@@ -1,35 +1,37 @@
 class simple_grid::nodes::lightweight_component::init(
   $mode = lookup('simple_grid::mode'),
   $augmented_site_level_config_file = lookup('simple_grid::components::yaml_compiler::output'),
-  $preferred_tech_stack_key = lookup("simple_grid::components::site_level_config_file::objects::preferred_tech_stack"),
-  $container_orchestration_key = lookup("simple_grid::components::site_level_config_file::objects::preferred_tech_stack::container_orchestration"),
-  $swarm_key = lookup("simple_grid::components::site_level_config_file::objects::preferred_tech_stack::container_orchestration::swarm"),
-  $kubernetes_key = lookup("simple_grid::components::site_level_config_file::objects::preferred_tech_stack::container_orchestration::kubernetes"),
-  $default_orchestrator = lookup("simple_grid::components::site_level_config_file::objects::preferred_tech_stack::container_orchestration::default"),
-  $cm_node_type = lookup("simple_grid::node_type:config_master"),
-  $lc_node_type = lookup("simple_grid::node_type:lightweight_component"),
+  $preferred_tech_stack_key = lookup('simple_grid::components::site_level_config_file::objects::preferred_tech_stack'),
+  $container_orchestration_key = lookup('simple_grid::components::site_level_config_file::objects::preferred_tech_stack::container_orchestration'),
+  $swarm_key = lookup('simple_grid::components::site_level_config_file::objects::preferred_tech_stack::container_orchestration::swarm'),
+  $kubernetes_key = lookup('simple_grid::components::site_level_config_file::objects::preferred_tech_stack::container_orchestration::kubernetes'),
+  $default_orchestrator = lookup('simple_grid::components::site_level_config_file::objects::preferred_tech_stack::container_orchestration::default'),
+  $cm_node_type = lookup('simple_grid::node_type:config_master'),
+  $lc_node_type = lookup('simple_grid::node_type:lightweight_component'),
 )
 {
   if $simple_stage == lookup('simple_grid::stage::install'){
-    class{"simple_grid::install::lightweight_component::init":}
-    simple_grid::components::execution_stage_manager::set_stage { "Setting stage to pre_deploy_step_1":
+    class{'simple_grid::install::lightweight_component::init':}
+    simple_grid::components::execution_stage_manager::set_stage { 'Setting stage to pre_deploy_step_1':
       simple_stage => lookup('simple_grid::stage::pre_deploy::step_1') #handled by tasks executed by CM
     }
   }
   elsif $simple_stage == lookup('simple_grid::stage::pre_deploy::step_1') {
-    simple_grid::components::execution_stage_manager::set_stage {"Setting stage to pre_deploy_step_2":
-       simple_stage => lookup('simple_grid::stage::pre_deploy::step_2') 
+    simple_grid::components::execution_stage_manager::set_stage {'Setting stage to pre_deploy_step_2':
+      simple_stage => lookup('simple_grid::stage::pre_deploy::step_2')
     }
   }
   elsif $simple_stage == lookup('simple_grid::stage::pre_deploy::step_2'){
-    class{"simple_grid::components::swarm::configure::firewall":}
-    class{"simple_grid::pre_deploy::lightweight_component::generate_deploy_status_file":}
-    class{"simple_grid::pre_deploy::lightweight_component::copy_augmented_site_level_config":}
-    class{"simple_grid::pre_deploy::lightweight_component::copy_additional_files":}
-    class{"simple_grid::pre_deploy::lightweight_component::copy_lifecycle_callbacks":}
-    class{"simple_grid::pre_deploy::lightweight_component::create_log_dirs":}
-    class{"simple_grid::pre_deploy::lightweight_component::copy_host_certificates":}
-    simple_grid::components::execution_stage_manager::set_stage {"Setting stage to pre_deploy_step_3":
+    class{'simple_grid::components::swarm::configure::firewall':}
+    class{'simple_grid::pre_deploy::lightweight_component::generate_deploy_status_file':}
+    class{'simple_grid::pre_deploy::lightweight_component::copy_augmented_site_level_config':}
+    class{'simple_grid::pre_deploy::lightweight_component::copy_additional_files':}
+    class{'simple_grid::pre_deploy::lightweight_component::scripts::generate_script_dir':}
+    class{'simple_grid::pre_deploy::lightweight_component::scripts::generate_script_wrappers':}
+    class{'simple_grid::pre_deploy::lightweight_component::scripts::copy_lifecycle_callbacks':}
+    class{'simple_grid::pre_deploy::lightweight_component::create_log_dirs':}
+    class{'simple_grid::pre_deploy::lightweight_component::copy_host_certificates':}
+    simple_grid::components::execution_stage_manager::set_stage {'Setting stage to pre_deploy_step_3':
       simple_stage => lookup('simple_grid::stage::pre_deploy::step_3')
     }
   }
@@ -37,7 +39,7 @@ class simple_grid::nodes::lightweight_component::init(
     include 'git'
     class{'simple_grid::ccm_function::config_orchestrator':}
     class{'simple_grid::pre_deploy::lightweight_component::download_component_repository':}
-    simple_grid::components::execution_stage_manager::set_stage {"Setting stage to deploy step 1":
+    simple_grid::components::execution_stage_manager::set_stage {'Setting stage to deploy step 1':
       simple_stage => lookup('simple_grid::stage::deploy::step_1') #handled by tasks executed by CM
     }
   }
---
+++ manifests/pre_deploy/lightweight_component/scripts.pp
@@ -0,0 +1,45 @@
+class simple_grid::pre_deploy::lightweight_component::scripts::generate_script_dir(
+  $scripts_dir = lookup('simple_grid::scripts_dir'),
+){
+  file{'Creating directory for scripts':
+    ensure => directory,
+    path   => $scripts_dir
+  }
+}
+
+class simple_grid::pre_deploy::lightweight_component::scripts::generate_script_wrappers(
+  $augmented_site_level_config_file = lookup('simple_grid::components::yaml_compiler::output'),
+  $scripts_dir = lookup('simple_grid::scripts_dir'),
+  $retry_command_wrapper = lookup('simple_grid::scripts::wrapper::retry'),
+  $lifecycle_wrapper = lookup('simple_grid::scripts::wrapper::lifecycle'),
+){
+  file { "Generate ${retry_command_wrapper}.sh":
+    ensure  => 'present',
+    path    => "${scripts_dir}/${retry_command_wrapper}",
+    mode    => '0555',
+    content => epp("simple_grid/${retry_command_wrapper}")
+  }
+  file { "Generate ${lifecycle_wrapper}":
+    ensure  => 'present',
+    path    => "${scripts_dir}/${lifecycle_wrapper}",
+    mode    => '0555',
+    content => epp("simple_grid/${retry_command_wrapper}")
+  }
+}
+
+class simple_grid::pre_deploy::lightweight_component::scripts::copy_lifecycle_callbacks(
+  $augmented_site_level_config_file = lookup('simple_grid::components::yaml_compiler::output'),
+  $scripts_dir = lookup('simple_grid::scripts_dir'),
+){
+  $execution_id_master_id_pairs = simple_grid::get_execution_ids($augmented_site_level_config_file, $fqdn)
+  notify{"Copying Lifecycle Callbacks on ${fqdn}":}
+  $execution_id_master_id_pairs.each |Integer $index, Hash $execution_id_master_id_pair| {
+    file{"Copying lifecycle callback scripts for execution id ${execution_id_master_id_pair['execution_id']}":
+      ensure  => directory,
+      recurse => 'remote',
+      source  => "puppet:///simple_grid/${scripts_dir}/${execution_id_master_id_pair['id']}",
+      path    => "${scripts_dir}/${execution_id_master_id_pair['execution_id']}",
+      mode    => '0766'
+    }
+  }
+}
